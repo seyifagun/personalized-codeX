13386fd00a6b88c7cc81c28987fdbf969457f083
seyi <seyifagun@gmail.com>
2022-12-26 17:32:50 +0100

    first commit

---
+++ server/server.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey : process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors()); //allows server to be called on the frontend
+app.use(express.json()); //allows us pass json from frontend to backend
+
+app.get('/', async(req, res) => {
+    res.status(200).send({
+        message: 'Hello from CodeX',
+    })
+});
+
+app.post('/', async(req, res) => {
+    try {
+        const prompt = req.body.prompt;
+
+        const response = await openai.createCompletion({
+            model : "text-davinci-003",
+            prompt : `${prompt}`,
+            temperature : 0,
+            max_tokens : 3000, //length of response
+            top_p : 1, 
+            frequency_penalty : 0.5, //not repeat similar answers often
+            presence_penalty:0,
+        });
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        })
+    } catch(error){
+        console.log(error);
+        res.status(500).send({error})
+    }
+})
+
+app.listen(5000, () => {console.log('Server is running on port http://localhost:5000 ')}) // run server
