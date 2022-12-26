fd14ba8dc1adfe12358fe3fdea074867eb94083a
seyi <seyifagun@gmail.com>
2022-12-26 18:20:59 +0100

    Modify server endpoint

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
 
     loader(messageDiv);
 
-    const response = await fetch('https://codex-p7i3.onrender.com/', {
+    const response = await fetch('https://codex-p7i3.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
